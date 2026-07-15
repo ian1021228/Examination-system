d838265ed3377845fe5e29b40fabe3599d1838e7
ian1021228 <ianw.solar@gmail.com>
2026-07-15 12:34:28 +0800

    123

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Examination-system-test/', // 加上這個讓 GitHub Pages 能夠正確載入 CSS/JS 資源
 })
 
